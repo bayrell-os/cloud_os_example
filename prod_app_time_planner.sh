fc2e212134f2858de194de3874d764f8c1df2c66
Ildar Bikmamatov <ildar@bayrell.org>
2020-11-01 17:29:28 +0600

    add time planner sh

---
+++ prod_app_time_planner.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f $0)
+SCRIPT_PATH=`dirname $SCRIPT`
+BASE_PATH=`dirname $SCRIPT_PATH`
+RETVAL=0
+
+case "$1" in
+	
+	compose)
+		docker stack deploy -c files/prod_app_time_planner.yaml prod --with-registry-auth
+	;;
+	
+	delete)
+		docker service rm prod_app_time_planner
+		docker service rm prod_app_time_planner_cron
+	;;
+	
+	recreate)
+		$0 delete
+		sleep 2
+		$0 compose
+	;;
+	
+	*)
+		echo "Usage: $0 {compose|delete|recreate}"
+		RETVAL=1
+	
+esac
+
+exit $RETVAL
