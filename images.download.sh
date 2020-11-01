7925681c487575a15e4bb0adb237ad9444043021
Ildar Bikmamatov <ildar@bayrell.org>
2020-11-01 17:38:52 +0600

    add download images script

---
+++ images.download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker image pull bayrell/alpine_mariadb:10.4
+docker images pull bayrell/cloud_os_installer:0.1.0
+docker images pull bayrell/cloud_os_standard:0.1.0
+docker images pull bayrell/cloud_router:0.1.0
+docker images pull bayrell/app_time_planner:0.1.0
